73f7d21a53ea028ca3b05ab1eacc01716ad975f6
kkwon <soonbin1997@gmail.com>
2022-08-27 08:34:38 +0900

    add coverage

--- main_test.py
+++ main_test.py
@@ -5,8 +5,11 @@ from main import hello
 
 class MainTest(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello(), "hello wwwworld")
+        self.assertEqual(hello(), "hello world")
 
 
 if __name__ == "__main__":
     unittest.main()
+    # python -m unittest discover -p "*_test.py"
+    # coverage run --source=./ -m unittest discover -p "*_test.py"
+    # coverage xml
